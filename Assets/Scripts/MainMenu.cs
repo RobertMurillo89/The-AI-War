026352688f06da8685ca7a56e48e24bbb2483e9f
Rillo813 <robert.murillo.89@gmail.com>
2023-12-03 23:54:21 -0800

    More work on mainmenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource MusicSource;
+    public AudioSource AmbianceSource;
+    public AudioSource SFXSource;
+    public AudioClip[] MainMenuMusic;
+    public AudioClip[] MainMenuAmbi;
+    public AudioClip[] MainMenuSFX;
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
