9363aeec470a32f3f163cbdb48080dc5f5617928
Rillo813 <robert.murillo.89@gmail.com>
2023-12-05 11:11:00 -0800

    got mele and ranged enemies working and damaging player

--- Assets/Scripts/Enemy_Projectile.cs
+++ Assets/Scripts/Enemy_Projectile.cs
@@ -2,20 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Projectile : Projectile
+public class Enemy_Projectile : MonoBehaviour
 {
-    protected override void OnTriggerEnter2D(Collider2D hitInfo)
-    {
-        if (hitInfo.gameObject.CompareTag("Player"))
-        {
+    public int Damage;
+    public float Speed;
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
 
-            IDamage damageable = hitInfo.GetComponent<IDamage>();
+
+
+    public void SetProperties(int damage, float speed, Vector3 direction, Sprite skin)
+    {
+        this.Damage = damage;
+        this.Speed = speed;
+        spriteRenderer.sprite = skin;
+
+        rb.velocity = direction * speed;
+    }
+
+    public void SetLayer(int layer)
+    {
+        gameObject.layer = layer;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Hit Player");
+            IDamage damageable = collision.gameObject.GetComponent<IDamage>();
             if (damageable != null)
             {
+                Debug.Log("Applying Damage");
                 damageable.takeDamage(Damage);
             }
+            else
+            {
+                Debug.Log("IDamage component not found on Player");
+            }
+
+            // Optionally add an impact effect or sound here
 
             Destroy(gameObject);
         }
+    }
+    void Update()
+    {
+        Debug.Log(transform.position);
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
