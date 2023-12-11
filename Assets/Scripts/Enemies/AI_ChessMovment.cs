2c84927eab6c9de34b755675bd66cbea354154f5
bigpallidbat <82740920+bigpallidbat@users.noreply.github.com>
2023-12-10 21:37:57 -0700

    some work on AI

--- Assets/Scripts/Enemies/AI_ChessMovment.cs
+++ Assets/Scripts/Enemies/AI_ChessMovment.cs
@@ -9,13 +9,14 @@ public class AI_Chess : EnemyAI
     Vector3 Direction;
     Vector2 DirectionCheackBox;
     Vector3 otherDirection;
+    Vector3 retreat;
     Vector2 otherCheackBox;
     bool isMoving;
     [SerializeField] int movement;
     [SerializeField] int moveTicks;
     [SerializeField] BoxCollider2D canIMove;
     [SerializeField] BoxCollider2D otherChecker;
-    bool iCantMave;
+    bool iCantMove = true;
     bool foundOtherWall;
     int ticks;
     int DiceRoll;
@@ -32,25 +33,28 @@ public class AI_Chess : EnemyAI
     // Update is called once per frame
     void Update()
     {
-        if (ticks > 0)
-        {
-            transform.Translate(Direction * movement * Time.deltaTime);
-            ticks--;
-        }
-        else
-        {
-            ticks = moveTicks;
-            Direction = SetDirection();
-        }
+        MoveTowardsPlayer();
     }
     protected override void MoveTowardsPlayer()
     {
         if (ticks > 0)
         {
-            if (!isMoving)
+            if (iCantMove)
             {
-            transform.Translate(Direction * movement * Time.deltaTime);
-            ticks--;
+                transform.Translate(Direction * movement * Time.deltaTime);
+                ticks--;
+            }
+            else
+            {
+                canIMove.offset = otherCheackBox;
+                if (iCantMove) transform.Translate(otherDirection * movement * Time.deltaTime);
+                else
+                {
+                    Direction = retreat;
+                    ticks = 5;
+                    transform.Translate(Direction * movement * Time.deltaTime);
+                }
+                canIMove.offset = DirectionCheackBox;
             }
         }
         else
@@ -64,66 +68,162 @@ public class AI_Chess : EnemyAI
         Vector3 VDirection = Vector3.zero;
         Vector2 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
+            DiceRoll = Random.Range(0, 2);
         if (angle >= -22.5f && angle < 22.5f)
         {
             VDirection = new Vector3(1, 0, 0).normalized;
             DirectionCheackBox = new Vector2(1, 0);
-            DiceRoll = Random.Range(0, 1);
-            //if (DiceRoll)
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(0, 1, 0).normalized;
+                otherCheackBox = new Vector2(0, 1);
+                retreat = new Vector3 (1,-1,0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(0, -1, 0).normalized;
+                otherCheackBox = new Vector2(0, -1);
+                retreat = new Vector3(1, 1, 0).normalized;
+            }
         }
         else if (angle >= 22.5f && angle < 67.5f)
         {
             VDirection = new Vector3(1, 1, 0).normalized;
             DirectionCheackBox = new Vector2(1, 1);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(-1, 1, 0).normalized;
+                otherCheackBox = new Vector2(-1, 1);
+                retreat = new Vector3(1, 0, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(1, -1, 0).normalized;
+                otherCheackBox = new Vector2(1, -1);
+                retreat = new Vector3(0, 1, 0).normalized;
+            }
         }
         else if (angle >= 67.5f && angle < 112.5f)
         {
             VDirection = new Vector3(0, 1, 0).normalized;
             DirectionCheackBox = new Vector2(0, 1);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(-1, 0, 0).normalized;
+                otherCheackBox = new Vector2(-1, 0);
+                retreat = new Vector3(1, 1, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(1, 0, 0).normalized;
+                otherCheackBox = new Vector2(1, 0);
+                retreat = new Vector3(-1, 1, 0).normalized;
+            }
         }
         else if (angle >= 112.5f && angle < 157.5f)
         {
             VDirection = new Vector3(-1, 1, 0).normalized;
             DirectionCheackBox = new Vector2(-1, 1);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(-1, -1, 0).normalized;
+                otherCheackBox = new Vector2(-1, -1);
+                retreat = new Vector3(0, 1, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(1, 1, 0).normalized;
+                otherCheackBox = new Vector2(1, 1);
+                retreat = new Vector3(-1, 0, 0).normalized;
+            }
         }
         else if (angle >= 157.5f || angle < -157.5f)
         {
             VDirection = new Vector3(-1, 0, 0).normalized;
             DirectionCheackBox = new Vector2(-1, 0);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(0, 1, 0).normalized;
+                otherCheackBox = new Vector2(0, 1);
+                retreat = new Vector3(1, 1, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(0, -1, 0).normalized;
+                otherCheackBox = new Vector2(0, -1);
+                retreat = new Vector3(1, -1, 0).normalized;
+            }
         }
         else if (angle >= -157.5f && angle < -112.5f)
         {
             VDirection = new Vector3(-1, -1, 0).normalized;
             DirectionCheackBox = new Vector2(-1, -1);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(-1, 1, 0).normalized;
+                otherCheackBox = new Vector2(-1, 1);
+                retreat = new Vector3(-1, 0, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(1, -1, 0).normalized;
+                otherCheackBox = new Vector2(1, -1);
+                retreat = new Vector3(0, -1, 0).normalized;
+            }
         }
         else if (angle >= -112.5f && angle < -67.5f)
         {
             VDirection = new Vector3(0, -1, 0).normalized;
             DirectionCheackBox = new Vector2(0, -1);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(-1, 0, 0).normalized;
+                otherCheackBox = new Vector2(-1, 0);
+                retreat = new Vector3(-1, -1, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(1, -0, 0).normalized;
+                otherCheackBox = new Vector2(1, -0);
+                retreat = new Vector3(1, -1, 0).normalized;
+            }
         }
         else if (angle >= -67.5f && angle < -22.5f)
         {
             VDirection = new Vector3(1, -1, 0).normalized;
             DirectionCheackBox = new Vector2(1, -1);
+            if (DiceRoll == 1)
+            {
+                otherDirection = new Vector3(1, 1, 0).normalized;
+                otherCheackBox = new Vector2(1, 1);
+                retreat = new Vector3(1, 0, 0).normalized;
+            }
+            else
+            {
+                otherDirection = new Vector3(-1, -1, 0).normalized;
+                otherCheackBox = new Vector2(-1, -1);
+                retreat = new Vector3(0, -1, 0).normalized;
+            }
         }
-            canIMove.offset = DirectionCheackBox;
+        canIMove.offset = DirectionCheackBox;
 
         // Default direction if no angle condition is met
         return VDirection;
     }
-    private void OnTriggerStay2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Collision with the wall!");
         if (other.CompareTag("wall"))
         {
-            iCantMave = true;
+            iCantMove = false;
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("wall"))
         {
-            iCantMave = false;
+            iCantMove = true;
         }
     }
 }
