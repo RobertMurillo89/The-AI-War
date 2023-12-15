e0b5dae6354edd5b0d3588d288a533ee6b3641a7
Rillo813 <robert.murillo.89@gmail.com>
2023-12-14 22:43:07 -0800

    sdfgsf

--- Assets/Scripts/Enemies/ChessPiece.cs
+++ Assets/Scripts/Enemies/ChessPiece.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChessPiece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string PieceType { get; set; }
+    public (int, int)[] Direction { get; set; }
+
+    public ChessPiece(string pieceType)
     {
-        
+        PieceType = pieceType;
+        SetDirection(PieceType);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetDirection(string pieceType)
     {
-        
+        switch (pieceType)
+        {
+            case "Rook":
+                Direction = new[] { (0, 1), (0, -1), (1, 0), (-1, 0) }; // Rook can move horizontally or vertically
+                break;
+            case "Bishop":
+                Direction = new[] { (1, 1), (1, -1), (-1, 1), (-1, -1) }; // Bishop can move diagonally
+                break;
+            case "Queen":
+                Direction = new[] { (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1) }; // Queen can move in any direction
+                break;
+            default:
+                throw new ArgumentException("Invalid piece type");
+        }
     }
 }
