a3d8e4cea9a654e76d754a0cead37481b23316a6
Rillo813 <robert.murillo.89@gmail.com>
2023-12-14 22:42:48 -0800

    cleaned up enemychess a little

--- Assets/Scripts/Enemies/AI_Chess.cs
+++ Assets/Scripts/Enemies/AI_Chess.cs
@@ -1,102 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine; // Importing the UnityEngine namespace
 
 public class AI_Chess : EnemyAI
 {
+
+    [Header("Player Reference")]
     public Transform player; // Reference to the player's Transform
-    Vector3 Direction;
-    Vector2 DirectionCheackBox;
-    Vector3 otherDirection;
-    Vector3 retreat;
-    Vector2 otherCheackBox;
 
-    bool isMoving;
-    [SerializeField] int movement;
-    [SerializeField] int moveTicks;
-    [SerializeField] BoxCollider2D canIMove;
-    [SerializeField] GameObject otherChecker;
-    //[SerializeField] Boxinllider2D otherChecker;
-    [SerializeField] byte typeOfMove; 
-    bool iCantMove = true;
-    bool foundOtherWall;
-    int ticks;
-    int DiceRoll;
-    bool randomBool;
-    public enum MoveType { Rook = 1, Bishop, Queen }
+    [Header("Movement Variables")]
+    [SerializeField] int movement; // Movement speed, exposed in the inspector
+    [SerializeField] int moveTicks; // Move ticks, exposed in the inspector
+    private Vector3 Direction; // Direction of movement
+    private Vector3 otherDirection; // Alternative direction of movement
+    private Vector3 retreat; // Direction to retreat
+    private bool isMoving; // Check if the AI is moving
+    private int ticks; // Ticks counter
+    private int DiceRoll; // Dice roll for randomness
 
+    [Header("Collision Detection")]
+    [SerializeField] BoxCollider2D canIMove; // Check if the AI can move, exposed in the inspector
+    [SerializeField] GameObject otherChecker; // Other checker object, exposed in the inspector
+    private Vector2 DirectionCheackBox; // Direction of the main check box
+    private Vector2 otherCheackBox; // Direction of the other check box
+    private bool iCantMove = true; // Check if the AI can't move
+    private bool foundOtherWall; // Check if another wall is found
 
-    // Update is called once per frame
-    // Start is called before the first frame update
-    //void Start()
-    //{
+    [Header("MoveType Types and Decision Making")]
+    [SerializeField] byte typeOfMove; // Type of move, exposed in the inspector
+    public enum MoveType { Rook = 1, Bishop, Queen } // Types of moves
+    private bool randomBool; // Random boolean for decision making
 
-    //ticks = moveTicks;
-    //}
-
-
-    // Update is called once per frame
-    void Update()
+    void Update() // Update method called once per frame
     {
-        MoveTowardsPlayer();
+        MoveTowardsPlayer(); // Calling the MoveTowardsPlayer method
     }
-    protected override void MoveTowardsPlayer()
+    protected override void MoveTowardsPlayer() // Overriding the MoveTowardsPlayer method from the EnemyAI class
     {
-        if (ticks > 0)
+        if (ticks > 0) // If ticks is greater than 0
         {
-            if (iCantMove)
+            if (iCantMove) // If the AI can't move
             {
-                foundOtherWall = false;
-                transform.Translate(Direction * movement * Time.deltaTime);
-                ticks--;
+                foundOtherWall = false; // Set foundOtherWall to false
+                transform.Translate(Direction * movement * Time.deltaTime); // Move the AI in the direction multiplied by the movement speed and the time since the last frame
+                ticks--; // Decrement ticks by 1
             }
-            else
+            else // If the AI can move
             {
-                if (!foundOtherWall) transform.Translate(otherDirection * movement * Time.deltaTime);
-                else
+                if (!foundOtherWall) transform.Translate(otherDirection * movement * Time.deltaTime); // If the AI hasn't found another wall, move the AI in the other direction multiplied by the movement speed and the time since the last frame
+                else // If the AI has found another wall
                 {
-                    Direction = retreat;
-                    ticks = 100;
-                    foundOtherWall = false;
-                    transform.Translate(Direction * movement * Time.deltaTime);
-                    ticks--;
+                    Direction = retreat; // Set the direction to the retreat direction
+                    ticks = 100; // Set ticks to 100
+                    foundOtherWall = false; // Set foundOtherWall to false
+                    transform.Translate(Direction * movement * Time.deltaTime); // Move the AI in the direction multiplied by the movement speed and the time since the last frame
+                    ticks--; // Decrement ticks by 1
                 }
-                //foundOtherWall = true;
-                //canIMove.offset = otherCheackBox;
-                //if
-                // {
-                //transform.Translate(Direction * movement * Time.deltaTime);
-                //}
-                //canIMove.offset = DirectionCheackBox;
             }
         }
-        else
+        else // If ticks is not greater than 0
         {
-            ticks = moveTicks;
-            switch (typeOfMove)
+            switch (typeOfMove) // Switch statement based on the type of move
             {
-                case 0:
-            Direction = SetDirectionRook();
-                    break;
-                    case 1:
-            Direction = SetDirectionBishop();
-                    break;
-                default:
-            Direction = SetDirectionQueen();
-                    break;
+                case 0: // Case 0
+                    Direction = SetDirectionRook(); // Set the direction to the result of the SetDirectionRook method
+                    break; // Break out of the switch statement
+                case 1: // Case 1
+                    Direction = SetDirectionBishop(); // Set the direction to the result of the SetDirectionBishop method
+                    break; // Break out of the switch statement
+                default: // Default case
+                    Direction = SetDirectionQueen(); // Set the direction to the result of the SetDirectionQueen method
+                    break; // Break out of the switch statement
             }
         }
     }
 
-    public void Retreat()
+    public void Retreat() // Public method to make the AI retreat
     {
-        foundOtherWall = true;
+        foundOtherWall = true; // Set foundOtherWall to true
     }
-    //void findPlayerAngle()
-    //{
 
-    //}
     Vector3 SetDirectionRook()
     {
         Vector3 VDirection = Vector3.zero;
@@ -399,19 +380,19 @@ public class AI_Chess : EnemyAI
         return VDirection;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) // Method called when the AI enters a 2D trigger
     {
-        Debug.Log("Collision with the wall!");
-        if (other.CompareTag("wall"))
+        Debug.Log("Collision with the wall!"); // Log a message to the console
+        if (other.CompareTag("wall")) // If the other collider has the tag "wall"
         {
-            iCantMove = false;
+            iCantMove = false; // Set iCantMove to false
         }
     }
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other) // Method called when the AI exits a 2D trigger
     {
-        if (other.CompareTag("wall"))
+        if (other.CompareTag("wall")) // If the other collider has the tag "wall"
         {
-            iCantMove = true;
+            iCantMove = true; // Set iCantMove to true
         }
     }
 }
